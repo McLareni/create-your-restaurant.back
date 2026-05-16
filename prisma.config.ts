31e93f943673d59cbc2a16fc13db1d94940ebf12
McLareni <mukola.kosobutski05@gmail.com>
2026-05-16 16:44:05 +0200

    fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "prisma/config";
+import 'dotenv/config';
 
-export default defineConfig({
-  schema: "prisma/schema.prisma",
+export default {
+  schema: 'prisma/schema.prisma',
   migrations: {
-    path: "prisma/migrations",
+    path: 'prisma/migrations',
   },
   datasource: {
-    url: process.env["DATABASE_URL"] as string,
+    url: process.env.DATABASE_URL,
   },
-});
+};
