7e7e9f72e57b7c450f9cb5586a66b714ba3e904c
Palacio2 <zaviyskiyxd@gmail.com>
2026-05-21 19:15:51 +0200

    fix: усунено останні помилки типізації аргументів та статусів Swagger

--- src/orders/dto/update-order.dto.ts
+++ src/orders/dto/update-order.dto.ts
@@ -1,17 +1,15 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { OrderStatus } from '@prisma/client';
-import { IsEnum, IsOptional, IsUUID } from 'class-validator';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 
 export class UpdateOrderDto {
-  @ApiPropertyOptional({ enum: OrderStatus, example: OrderStatus.IN_PROGRESS })
+  @ApiPropertyOptional({ enum: OrderStatus, example: OrderStatus.COOKING })
   @IsOptional()
   @IsEnum(OrderStatus)
   status?: OrderStatus;
-  @ApiPropertyOptional({
-    example: '1a2d7d9c-5f73-4bf0-b89a-f12474a584d3',
-    description: 'Dining table ID',
-  })
+
+  @ApiPropertyOptional({ example: 'uuid-table-id' })
   @IsOptional()
-  @IsUUID()
+  @IsString()
   tableId?: string;
 }
